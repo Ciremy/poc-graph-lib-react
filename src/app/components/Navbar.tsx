984c2c3044f7a5d2a580584f8a8c62bb3c78d0bb
Sangmin <sangmin@knock-knock.fr>
2024-03-14 15:45:59 +0100

    basic page design

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='absolute top-0 w-full flex justify-between bg-primary text-tertiary font-bold p-5'>
+      <Link href="/visx">
+        Visx
+      </Link>
+      <Link href="/example2">
+        Example 2
+      </Link>
+      <Link href="/example3">
+        Example 3
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,47 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    
+      colors: {
+        // -------------
+        // PRINCIPAL
+        // -------------
+        primary: '#1C3144',
+        secondary: '#346C80',
+        tertiary: '#BCE8F0',
+        card: '#4290A4',
+        background: '#142331',
+        success: '#69B876',
+        danger: '#D93939',
+        warn: '#FF8B1C',
+        beware: '#D4BC65',
+        gray: '#7E989D',
+        lightgreen: '#58C5D9',
+        lightblue: '#50c8ff',
+        light: '#a4adb4',
+
+        // -------------
+        // DEACTIVATED
+        // -------------
+        successDeactivated: 'rgb(105 184 118/0.3)',
+        dangerDeactivated: 'rgb(217 57 57 / 0.3)',
+        bewareDeactivated: 'rgb(212 188 101/0.3)',
+
+        // -------------
+        // HOVER
+        // -------------
+        hoverPrimary: '#264755',
+        hoverSuccess: '#569a64',
+        hoverSucessDeactivated: 'rgb(105 184 118/0.5)',
+        hoverDanger: '#a82e2e',
+        hoverDangerDeactivated: 'rgb(217 57 57 / 0.5)',
+        hoverBeware: '#b4a14d',
+        hoverBewareDeactivated: 'rgb(212 188 101/0.5)',
+        hoverTertiary: '#6FA2B8',
+        hoverSecondary: '#4E8FA9',
+        hoverLight: '#b6bfc6',
+        toggleButton: '#D2D6DA',
+      },
     },
   },
   plugins: [],
