77e0e4e66b1e53c4200b5a47f0671c4dbe020195
Sangmin <sangmin@knock-knock.fr>
2024-07-23 11:45:14 +0200

    deleted

--- src/app/react-flow/page.tsx
+++ src/app/react-flow/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <div className="flex h-screen flex-col justify-center items-center">
       <Navbar/>
-
         <div className="w-full h-4/5 mb-12">
             <h1 className="text-center font-bold text-2xl">React Flow</h1>    
             <div>
@@ -29,7 +28,6 @@ export default function Home() {
                     <AppCustom/>
                 )
             }
-
         </div>
     </div>
   );
